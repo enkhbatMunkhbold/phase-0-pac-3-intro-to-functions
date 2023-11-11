c1c9a7268424f8285d523f9c3ca2698711c3adeb
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2023-11-10 19:40:14 -0800

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Follow along with the examples here
+// function sayHelloTo(firstName) {
+//     console.log(`Hello, ${firstName}`);
+// }
+
+// sayHelloTo("Guadalupe");
+// sayHelloTo("Jane");
+// sayHelloTo("R2-D2");
+// sayHelloTo(1);
+
+function subtraction(x, y) {
+    if (x > y) {
+        return x - y
+    } else {
+        return y - x
+    }
+}
+
+const difference = subtraction(4, 10)
+// console.log(difference)
+const message = `The difference between 4 and 10 is ${difference}`
+console.log(message)
